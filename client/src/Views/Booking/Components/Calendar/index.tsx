bb6647efea8a5df1ac5521f0bbda6bdbd0ad8e9e
Marcin Miler <marcin4951@gmail.com>
2018-07-13 13:36:01 +0200

    Items in calendar

--- client/src/Views/Booking/Components/Calendar/index.tsx
+++ client/src/Views/Booking/Components/Calendar/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
-import { Container, Row, Column } from './style'
+import * as moment from 'moment'
+
+import { Container, Row, Column, Legend, Item, ItemTitle } from './style'
 
 export class Calendar extends React.Component<{}, {}> {
     public render() {
@@ -13,17 +15,32 @@ export class Calendar extends React.Component<{}, {}> {
             'Saturday',
             'Sunday'
         ]
+
         const array = Array.from(Array(10).keys())
         const array2 = Array.from(Array(8).keys())
+        const currentDayOfWeek = parseInt(moment().format('e'), 10)
+
         return (
             <Container>
                 <Row>
-                    {days.map((day, i) => <Column key={i}>{day}</Column>)}
+                    {days.map((day, i) => <Legend key={i}>{day}</Legend>)}
                 </Row>
                 {array.map(item1 => (
                     <Row key={item1}>
-                        {array2.map(item2 => (
-                            <Column key={Date.now()}>{item2}</Column>
+                        {array2.map((item2, i) => (
+                            <Column
+                                key={Math.random()}
+                                active={i === currentDayOfWeek}
+                            >
+                                {item1 === 1 &&
+                                    item2 === 1 && (
+                                        <Item length={3}>
+                                            <ItemTitle>
+                                                John Dohn (3 nights)
+                                            </ItemTitle>
+                                        </Item>
+                                    )}
+                            </Column>
                         ))}
                     </Row>
                 ))}
--- client/src/Views/Booking/Components/Calendar/style.tsx
+++ client/src/Views/Booking/Components/Calendar/style.tsx
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
+interface ItemProps {
+    length: number
+}
+interface ColumnProps {
+    active?: boolean
+}
+
 export const Container = styled.div`
     width: 100%;
     flex-grow: 1;
@@ -10,13 +17,41 @@ export const Row = styled.div`
     display: flex;
     flex-direction: row;
 `
-export const Column = styled.div`
+export const Column = styled.div<ColumnProps>`
     width: 12.5%;
     height: 50px;
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative;
+    background-color: ${({ active }) => (active ? '#e9f6fb' : '#fbfcfc')};
     font-size: 18px;
-    border-right: 1px solid #efefef;
-    border-bottom: 1px solid #efefef;
+    border-right: 2px solid #efefef;
+    border-bottom: 2px solid #efefef;
+`
+export const Legend = Column.extend`
+    background-color: white;
+`
+export const Item = styled.div.attrs<ItemProps>({
+    width: ({ length }: ItemProps) => (length ? length : 1)
+})`
+    width: ${({ length }) => 100.5 * length + '%'};
+    height: 50px;
+    padding: 0 20px 0 20px;
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 100px;
+    border-radius: 2px;
+    background-color: white;
+    border-top: 3px solid #8090cd;
+    box-shadow: 0px 0px 5px #e5e5e5;
+    z-index: 100;
+    box-sizing: border-box;
+`
+export const ItemTitle = styled.p`
+    color: black;
+    font-size: 15px;
+    font-weight: 500;
 `
