e25489afe89708ecc2b239dc5d08ef52097dabda
Marcin Miler <marcin4951@gmail.com>
2018-07-26 23:31:10 +0200

    Serivces UI

--- client/src/Views/Booking/AddBooking/Components/Services/index.tsx
+++ client/src/Views/Booking/AddBooking/Components/Services/index.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react'
-import { Container, Title } from './style'
+import { Container, Title, Row, Text, List } from './style'
 import { Checkbox } from '../../../../../Components/Checkbox'
 
 export const Services: React.SFC<{}> = () => (
     <Container>
         <Title>Services</Title>
+        <List>
+            <Row>
+                <Checkbox onClick={() => null} />
+                <Text>Airport pickup / $10 - per person</Text>
+            </Row>
 
-        <Checkbox onClick={() => null} />
+            <Row>
+                <Checkbox onClick={() => null} />
+                <Text>Parking / 5$ - per day</Text>
+            </Row>
+
+            <Row>
+                <Checkbox onClick={() => null} />
+                <Text>Breakfast / $7 - per day</Text>
+            </Row>
+
+            <Row>
+                <Checkbox onClick={() => null} />
+                <Text>Wifi / $15</Text>
+            </Row>
+        </List>
     </Container>
 )
--- client/src/Views/Booking/AddBooking/Components/Services/style.tsx
+++ client/src/Views/Booking/AddBooking/Components/Services/style.tsx
@@ -13,3 +13,17 @@ export const Title = styled.h1`
     color: black;
     font-size: 18px;
 `
+export const List = styled.div`
+    margin-top: 20px;
+`
+export const Row = styled.div`
+    margin: 15px 0 15px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+export const Text = styled.p`
+    margin-left: 20px;
+    color: black;
+    font-weight: 500;
+`
